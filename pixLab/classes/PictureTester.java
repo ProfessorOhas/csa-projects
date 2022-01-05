fbb88152d9f256ce6c5b7aa498778dc7c140d964
OJ <ojaschaturvedi2006@gmail.com>
2022-01-05 16:47:55 -0700

    Work on these 2 methods

--- pixLab/classes/PictureTester.java
+++ pixLab/classes/PictureTester.java
@@ -95,6 +95,22 @@ public class PictureTester {
     temple.explore();
   }
 
+  /** Method to test mirrorArms */
+  public static void testMirrorArms() {
+    Picture snowman = new Picture("snowman.jpg");
+    snowman.explore();
+    snowman.mirrorArms();
+    snowman.explore();
+  }
+
+  /** Method to test mirrorGull */
+  public static void testMirrorGull() {
+    Picture seagull = new Picture("seagull.jpg");
+    seagull.explore();
+    seagull.mirrorGull();
+    seagull.explore();
+  }
+
   /** Method to test the collage method */
   public static void testCollage() {
     Picture canvas = new Picture("640x480.jpg");
