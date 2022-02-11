caad924a958fb2ae41da69bc26fdd9d4d47b3185
OJ <ojaschaturvedi2006@gmail.com>
2022-02-10 19:23:09 -0700

    Tried some stuff

--- Others/src/Others.java
+++ Others/src/Others.java
@@ -2,7 +2,12 @@
 public class Others {
 
 	public static void main(String[] args) {
-		
+		Others sloth = new Others();
+		System.out.println(sloth);
+	}
+
+	public String toString() {
+		return "This is an example of a toString method.";
 	}
 
 }
